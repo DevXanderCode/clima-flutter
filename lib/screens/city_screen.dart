96a1d5aff953164337da09bd6fc5d9b438ba06e8
Alex Chinedu <nwekealexchinedu@gmail.com>
2022-01-16 12:04:59 +0100

    started working on the city screen

---
+++ lib/screens/city_screen.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+import 'package:clima/utilities/contants.dart';
+
+class CityScreen extends StatefulWidget {
+  const CityScreen({Key? key}) : super(key: key);
+
+  @override
+  _CityScreenState createState() => _CityScreenState();
+}
+
+class _CityScreenState extends State<CityScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        decoration: const BoxDecoration(
+          image: DecorationImage(
+            image: AssetImage('images/city_background.png'),
+          ),
+        ),
+      ),
+    );
+  }
+}
