9b147f22e38d0edabe10869684dbc40cf11a6245
Alex Chinedu <nwekealexchinedu@gmail.com>
2022-01-22 11:25:33 +0100

    created the weather file

--- lib/screens/loading_screen.dart
+++ lib/screens/loading_screen.dart
@@ -14,8 +14,6 @@ class LoadingScreen extends StatefulWidget {
 }
 
 class _LoadingScreenState extends State<LoadingScreen> {
-  late double latitude;
-  late double longitude;
   @override
   void initState() {
     super.initState();
@@ -25,10 +23,9 @@ class _LoadingScreenState extends State<LoadingScreen> {
   void getLocationData() async {
     Location location = Location();
     await location.getCurrentLocation();
-    latitude = location.latitude;
-    longitude = location.longitude;
+
     NetworkHelper networkHelper = NetworkHelper(
-        'https://api.openweathermap.org/data/2.5/weather?lat=$latitude&lon=$longitude&appid=$apiKey&units=metric');
+        'https://api.openweathermap.org/data/2.5/weather?lat=${location.latitude}&lon=${location.longitude}&appid=$apiKey&units=metric');
     var weatherData = await networkHelper.getData();
 
     Navigator.push(
