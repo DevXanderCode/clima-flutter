4772c1217a93e172612e1d5b3d48eb69a8acd18a
Alex Chinedu <nwekealexchinedu@gmail.com>
2022-01-22 14:41:39 +0100

    created a function to get the type location weather

--- lib/screens/location_screen.dart
+++ lib/screens/location_screen.dart
@@ -84,7 +84,11 @@ class _LocationScreenState extends State<LocationScreen> {
                           builder: (context) => const CityScreen(),
                         ),
                       );
-                      print(typedName);
+                      if (typedName != null) {
+                        var weatherData =
+                            await weather.getCityWeather(typedName);
+                        updateUI(weatherData);
+                      }
                     },
                     child: const Icon(
                       Icons.location_city,
--- lib/services/weather.dart
+++ lib/services/weather.dart
@@ -5,6 +5,12 @@ const apiKey = 'dd558343d3f6126f2adffdf1c28522b1';
 const openWeatherMapURL = 'https://api.openweathermap.org/data/2.5/weather';
 
 class WeatherModel {
+  Future<dynamic> getCityWeather(cityName) async {
+    NetworkHelper networkHelper = NetworkHelper(
+        '$openWeatherMapURL?q=$cityName&appid=$apiKey&units=metric');
+    return await networkHelper.getData();
+  }
+
   Future<dynamic> getLocationWeather() async {
     Location location = Location();
     await location.getCurrentLocation();
