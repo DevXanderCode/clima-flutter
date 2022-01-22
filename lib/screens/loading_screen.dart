cf4d60e5c4e3b70ec72365716233941b11c6d90a
Alex Chinedu <nwekealexchinedu@gmail.com>
2022-01-22 10:51:10 +0100

    navigated to the location screen after getting the weather condition of the users current location

--- lib/screens/loading_screen.dart
+++ lib/screens/loading_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:clima/services/location.dart';
 import 'package:clima/services/networking.dart';
+import 'package:clima/screens/location_screen.dart';
 
 const apiKey = 'dd558343d3f6126f2adffdf1c28522b1';
 
@@ -28,6 +29,9 @@ class _LoadingScreenState extends State<LoadingScreen> {
     NetworkHelper networkHelper = NetworkHelper(
         'https://api.openweathermap.org/data/2.5/weather?lat=$latitude&lon=$longitude&appid=$apiKey');
     var weatherData = await networkHelper.getData();
+
+    Navigator.push(context,
+        MaterialPageRoute(builder: (context) => const LocationScreen()));
   }
 
   @override
